7a6585e49213171a20daf25acd59909f77cb33ed
Jon Kirkman <jonkirkman@gmail.com>
2014-09-27 22:39:22 -0400

    position helpers for  the player model

--- lib/src/arena.dart
+++ lib/src/arena.dart
@@ -1,10 +1,12 @@
 part of tron_hub;
 
-// Battle takes place in an Arena
+/**
+ * Battle takes place in an Arena
+ *
+ * Basically a 2D grid used to keep track of availability of Coordinates.
+ */
 class Arena {
-  int width;
-  int height;
-
+  int width, height;
   Map<String, bool> cells;
 
   Arena();
--- lib/src/player.dart
+++ lib/src/player.dart
@@ -2,6 +2,23 @@ part of tron_hub;
 
 // Who will battle?
 class Player {
+  String name;
+  bool active;
+  Queue<Coordinate> positions;
 
-  Player();
+  Player(this.name);
+
+  Coordinate get currentPosition => positions.first;
+             set currentPosition(Coordinate pos) => positions.addFirst(pos);
+
+  Coordinate get previousPosition = positions.elementAt(1);
+
+  String get bearing {
+    if (currentPosition.x == previousPosition.x) {
+      return (currentPosition.y > previousPosition.y)? "UP" : "DOWN";
+    }
+    else if (currentPosition.y == previousPosition.y) {
+      return (currentPosition.x > previousPosition.x)? "LEFT" : "RIGHT";
+    }
+  }
 }
