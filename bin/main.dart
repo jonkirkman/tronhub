0e0a3b7c1ea912602783e87ea0dd2f327344c254
Jon Kirkman <jonkirkman@gmail.com>
2014-11-11 21:59:05 -0500

    Basically just tinkering.

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,16 @@
+import "dart:io";
+import "package:tron_hub/tron_hub.dart" as Tron;
+
 main() {
-  print('Hello world!');
+    // start a webserver
+    // (laterz) maybe use game/[somegame] to allow peeps to create and join the same game
+    // listen for websocket connections
+    // convert websocket clients to players!
+    // when do we start the game?
+  var myArena = new Tron.Arena(200,200);
+  var players = [
+      new Tron.Player("JonBot")
+    ];
+
+  new Tron.Game(myArena, players);
 }
