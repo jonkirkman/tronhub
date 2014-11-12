186c01692db9f07ef5a85e63831b38f46296faa7
Jon Kirkman <jonkirkman@gmail.com>
2014-11-11 21:58:41 -0500

    just some of the basics for Game

--- lib/src/game.dart
+++ lib/src/game.dart
@@ -5,9 +5,22 @@ class Game {
   Arena arena;
   List<Player> players;
 
-  Game(this.arena, this.players);
+  Game(Arena arena, List<Player> players) {
+    this.arena = arena;
+    this.players = players;
+
+    gameLoop();
+  }
+
+  List<Player> get activePlayers {
+    return players.where( (p) => p.active );
+  }
 
   void gameLoop() {
-
+    // while we have active players
+    while(activePlayers.length >= 1) {
+      // give each players an opportunity to move
+      print("hey");
+    }
   }
 }
